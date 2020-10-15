9e7fe48b8a65489f5fb609081daca9066ee5337b
mbuscaratti <marcelo.buscaratti@outlook.com>
2020-10-15 18:57:58 -0300

    Moving files
    Create assets folder for images, audio etc.
    
    Create components folder for react components

---
+++ src/components/Calculator/Calculator.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Calculator.css';
+
+function Calculator() {
+  return (
+    <div className="Calculator">
+      In progress
+    </div>
+  );
+}
+
+export default Calculator;
